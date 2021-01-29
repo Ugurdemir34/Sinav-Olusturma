8320080888ca86383c227fb4498cbd23b4bd43ed
ugurdemir <ugurdemir551@gmail.com>
2021-01-30 01:38:04 +0300

    Session Added

--- Sinav-Olusturma/Startup.cs
+++ Sinav-Olusturma/Startup.cs
@@ -2,8 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -17,6 +19,10 @@ namespace Sinav_Olusturma
     {
         public Startup(IConfiguration configuration)
         {
+            using (var client = new SinavContext())
+            {
+                client.Database.EnsureCreated();
+            }
             Configuration = configuration;
         }
 
@@ -25,8 +31,35 @@ namespace Sinav_Olusturma
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddEntityFrameworkSqlite().AddDbContext<SinavContext>();
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.LoginPath = "/Auth/Login";
+                options.LogoutPath = "/Login/Logout";
+                options.AccessDeniedPath = "/Account/AccessDenied";
+                options.ExpireTimeSpan = TimeSpan.FromMinutes(30);
+                options.Cookie = new Microsoft.AspNetCore.Http.CookieBuilder
+                {
+                    HttpOnly = true,
+                    Name = "Sinav.Cookie",
+                    Path = "/",
+                    SameSite = SameSiteMode.Lax,
+                    SecurePolicy = CookieSecurePolicy.SameAsRequest
+                };
+            });
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+           .AddCookie(options =>
+           {
+               options.LoginPath = "/Auth/Login/";
+               options.LogoutPath = "/Auth/Logout/";
+               options.ExpireTimeSpan = TimeSpan.FromMinutes(30);
+           });
             services.AddControllersWithViews();
-           
+            services.AddSession();
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddDistributedMemoryCache();
+            services.AddHttpContextAccessor();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,6 +75,8 @@ namespace Sinav_Olusturma
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            app.UseSession();
+            app.UseAuthentication();
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
