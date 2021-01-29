3c46d736748a1fc686b4aff20c8f87894aa25969
ugurdemir <ugurdemir551@gmail.com>
2021-01-30 00:16:57 +0300

    Migrations Created

---
+++ Sinav-Olusturma.DataAccess/Migrations/20210129194337_InitialCreate.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Sinav_Olusturma.DataAccess.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Stories",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    Title = table.Column<string>(nullable: true),
+                    Content = table.Column<string>(nullable: true),
+                    Thumbnail = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    Link = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Stories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(nullable: true),
+                    Surname = table.Column<string>(nullable: true),
+                    Username = table.Column<string>(nullable: true),
+                    Password = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Questions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    StoryId = table.Column<int>(nullable: false),
+                    OptionA = table.Column<string>(nullable: true),
+                    OptionB = table.Column<string>(nullable: true),
+                    OptionC = table.Column<string>(nullable: true),
+                    OptionD = table.Column<string>(nullable: true),
+                    CorrectOption = table.Column<string>(nullable: true),
+                    StoryId1 = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Questions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Questions_Stories_StoryId1",
+                        column: x => x.StoryId1,
+                        principalTable: "Stories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Questions_StoryId1",
+                table: "Questions",
+                column: "StoryId1");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Questions");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Stories");
+        }
+    }
+}
---
+++ Sinav-Olusturma.DataAccess/Migrations/20210129194657_InitialCreateFixed.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Sinav_Olusturma.DataAccess.Migrations
+{
+    public partial class InitialCreateFixed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Questions_Stories_StoryId1",
+                table: "Questions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Questions_StoryId1",
+                table: "Questions");
+
+            migrationBuilder.DropColumn(
+                name: "StoryId1",
+                table: "Questions");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "StoryId",
+                table: "Questions",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Questions_StoryId",
+                table: "Questions",
+                column: "StoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Questions_Stories_StoryId",
+                table: "Questions",
+                column: "StoryId",
+                principalTable: "Stories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Questions_Stories_StoryId",
+                table: "Questions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Questions_StoryId",
+                table: "Questions");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "StoryId",
+                table: "Questions",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "StoryId1",
+                table: "Questions",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Questions_StoryId1",
+                table: "Questions",
+                column: "StoryId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Questions_Stories_StoryId1",
+                table: "Questions",
+                column: "StoryId1",
+                principalTable: "Stories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
