01be6c11c02fa3b332036ee4894aaece88ff5685
ugurdemir <ugurdemir551@gmail.com>
2021-01-30 00:50:58 +0300

    User Dal Created

---
+++ Sinav-Olusturma.DataAccess/Concrete/EntityFramework/EfUserDal.cs
@@ -0,0 +1,13 @@
+﻿using Sinav_Olusturma.Core.DataAccess.EntityFramework;
+using Sinav_Olusturma.DataAccess.Abstract;
+using Sinav_Olusturma.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sinav_Olusturma.DataAccess.Concrete.EntityFramework
+{
+    public class EfUserDal : EfEntityRepositoryBase<User,SinavContext>,IUserDal
+    {
+    }
+}
