68d1138f6246c16fae0e4e2a9951ec9fc4748fcc
ugurdemir <ugurdemir551@gmail.com>
2021-01-30 00:48:36 +0300

    Autofac Created

---
+++ Sinav-Olusturma.Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,23 @@
+﻿using Autofac;
+using Sinav_Olusturma.Business.Abstract;
+using Sinav_Olusturma.Business.Concrete;
+using Sinav_Olusturma.DataAccess.Abstract;
+using Sinav_Olusturma.DataAccess.Concrete.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sinav_Olusturma.Business.DependencyResolvers.Autofac
+{
+    public class AutofacBusinessModule:Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<UserManager>().As<IUserService>();
+            builder.RegisterType<EfUserDal>().As<IUserDal>();
+
+            builder.RegisterType<AuthManager>().As<IAuthService>();
+          
+        }
+    }
+}
