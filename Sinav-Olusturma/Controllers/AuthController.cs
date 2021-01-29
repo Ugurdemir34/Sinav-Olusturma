3c2190a425877b57eefa865243dc09c04bfaa976
ugurdemir <ugurdemir551@gmail.com>
2021-01-30 01:38:39 +0300

    Login action created

--- Sinav-Olusturma/Controllers/AuthController.cs
+++ Sinav-Olusturma/Controllers/AuthController.cs
@@ -1,25 +1,57 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Sinav_Olusturma.Business.Abstract;
+using Sinav_Olusturma.DataAccess.Concrete.EntityFramework;
 using Sinav_Olusturma.Entities.Dtos;
 
 namespace Sinav_Olusturma.Controllers
 {
     public class AuthController : Controller
     {
+       
+        private IAuthService _authService;
+        private IHttpContextAccessor _httpContextAccessor;
+        public AuthController(IHttpContextAccessor httpContextAccessor, IAuthService authService)
+        {
+            _authService = authService;          
+            _httpContextAccessor = httpContextAccessor;
+        }
         public IActionResult Index()
         {
             return View();
         }
         public IActionResult Login()
         {
+            var model = new UserForLoginDto();
+            var context = new SinavContext();
+            var mdel = context.Users.Where(i=>i.Id==1).FirstOrDefault();
             return View();
         }
         [HttpPost]
         public IActionResult Login(UserForLoginDto model)
         {
+            var result = _authService.Login(model);
+            if (result != null)
+            {
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name,model.Username)
+                };
+                var userIdentity = new ClaimsIdentity(claims, "login");
+                ClaimsPrincipal principal = new ClaimsPrincipal(userIdentity);
+                _httpContextAccessor.HttpContext.Session.SetString("username", result.Username);
+                ViewData["mesaj"] = "";
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewData["mesaj"] = "Kullanıcı Adı veya Parola Hatalı !";
+            }
             return View();
         }
     }
