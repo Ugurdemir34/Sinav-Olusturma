e81f337eee79ea41cb9f21b96077e5ef1c10805e
ugurdemir <ugurdemir551@gmail.com>
2021-01-30 00:15:56 +0300

    Auth Service Created

---
+++ Sinav-Olusturma.Business/Abstract/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sinav_Olusturma.Business.Abstract
+{
+    interface IAuthService
+    {
+    }
+}
---
+++ Sinav-Olusturma.Business/Concrete/AuthManager.cs
@@ -0,0 +1,24 @@
+﻿using Sinav_Olusturma.Business.Abstract;
+using Sinav_Olusturma.Entities.Concrete;
+using Sinav_Olusturma.Entities.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sinav_Olusturma.Business.Concrete
+{
+    public class AuthManager : IAuthService
+    {
+        private IUserService _userService;
+        public AuthManager(IUserService userService)
+        {
+            _userService = userService;
+        }
+        public User Login(UserForLoginDto userForLoginDto)
+        {
+            var userToCheck = _userService.GetAdmin(userForLoginDto.Username, userForLoginDto.Password);
+            return userToCheck;
+
+        }
+    }
+}
