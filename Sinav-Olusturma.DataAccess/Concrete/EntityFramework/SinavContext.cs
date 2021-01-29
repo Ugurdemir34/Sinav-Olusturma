a2804028b34a4dcb49703b6e085a52280fabbd1c
ugurdemir <ugurdemir551@gmail.com>
2021-01-30 00:49:16 +0300

    Context Created

---
+++ Sinav-Olusturma.DataAccess/Concrete/EntityFramework/SinavContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sinav_Olusturma.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sinav_Olusturma.DataAccess.Concrete.EntityFramework
+{
+    public class SinavContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Filename=SinavDB.db");
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Story> Stories { get; set; }
+        public DbSet<Question> Questions { get; set; }
+    }
+}
