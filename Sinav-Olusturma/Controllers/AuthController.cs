04588d9d9c4142ae7fd2e15f2b2f19028d975cb0
ugurdemir <ugurdemir551@gmail.com>
2021-01-30 00:15:36 +0300

    Auth Controller Created

---
+++ Sinav-Olusturma/Controllers/AuthController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Sinav_Olusturma.Entities.Dtos;
+
+namespace Sinav_Olusturma.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(UserForLoginDto model)
+        {
+            return View();
+        }
+    }
+}
