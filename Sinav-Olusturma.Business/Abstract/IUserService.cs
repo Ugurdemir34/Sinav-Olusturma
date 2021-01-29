7007da2c080aa3598e7ae05cac6e5fc9014398d4
ugurdemir <ugurdemir551@gmail.com>
2021-01-30 00:14:31 +0300

    User Service Created

---
+++ Sinav-Olusturma.Business/Abstract/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using Sinav_Olusturma.Entities.Concrete;
+using Sinav_Olusturma.Entities.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sinav_Olusturma.Business.Abstract
+{
+    public interface IUserService
+    {
+        User GetAdmin(string username, string password);
+    }
+}
---
+++ Sinav-Olusturma.Business/Concrete/UserManager.cs
@@ -0,0 +1,28 @@
+﻿using Sinav_Olusturma.Business.Abstract;
+using Sinav_Olusturma.DataAccess.Abstract;
+using Sinav_Olusturma.Entities.Concrete;
+using Sinav_Olusturma.Entities.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sinav_Olusturma.Business.Concrete
+{
+    public class UserManager : IUserService
+    {
+        private IUserDal _userDal;
+        public UserManager(IUserDal userDal)
+        {
+            _userDal = userDal;
+        }
+        public User GetAdmin(string username, string password)
+        {
+            var user = _userDal.Get(i => i.Username == username && i.Password == password);
+            return user;
+            
+            
+        }
+
+       
+    }
+}
