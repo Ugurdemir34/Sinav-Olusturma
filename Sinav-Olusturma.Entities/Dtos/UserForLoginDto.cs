573778063638c02b6e01e8278bc54ad1f169b884
ugurdemir <ugurdemir551@gmail.com>
2021-01-30 00:15:12 +0300

    Dto Created

---
+++ Sinav-Olusturma.Entities/Dtos/UserForLoginDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sinav_Olusturma.Entities.Dtos
+{
+    public class UserForLoginDto
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        
+    }
+}
