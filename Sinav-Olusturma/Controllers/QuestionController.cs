d479f4a8617d70b98715ff0a325256bc5abc2495
ugurdemir <ugurdemir551@gmail.com>
2021-01-30 02:19:47 +0300

    Question Controller Created

---
+++ Sinav-Olusturma/Controllers/QuestionController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Sinav_Olusturma.Controllers
+{
+    public class QuestionController : Controller
+    {
+        public IActionResult Index(string url)
+        {
+            ViewData["link"] = url;
+            return View();
+        }
+    }
+}
