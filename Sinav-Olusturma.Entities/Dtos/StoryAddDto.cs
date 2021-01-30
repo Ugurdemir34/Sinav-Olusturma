226540a49c6489130586f040aa782f699f29b881
ugurdemir <ugurdemir551@gmail.com>
2021-01-30 03:38:23 +0300

    Created

---
+++ Sinav-Olusturma.Entities/Dtos/StoryAddDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sinav_Olusturma.Entities.Dtos
+{
+    public class StoryAddDto
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Thumbnail { get; set; }
+        public string Description { get; set; }
+        public string Link { get; set; }
+    }
+}
