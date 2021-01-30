3888f3eed280aaf7d057523e83b78108586708e2
ugurdemir <ugurdemir551@gmail.com>
2021-01-30 04:25:21 +0300

    Created

---
+++ Sinav-Olusturma/Models/NewQuestionModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Sinav_Olusturma.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sinav_Olusturma.Models
+{
+    public class NewQuestionModel
+    {
+        public string StoryTitle { get; set; }
+        public string StoryContent { get; set; }
+        public List<Question> Questions { get; set; }
+        public List<SelectListItem> StoryTitles { get; set; }
+    }
+}
