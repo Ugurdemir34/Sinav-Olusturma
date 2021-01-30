83da6dfcad9a473f97be99a9ff47f5f0b10f29f7
ugurdemir <ugurdemir551@gmail.com>
2021-01-30 20:26:22 +0300

    Updated

--- Sinav-Olusturma.Entities/Dtos/StoryAddDto.cs
+++ Sinav-Olusturma.Entities/Dtos/StoryAddDto.cs
@@ -1,15 +1,16 @@
-﻿using System;
+﻿using Sinav_Olusturma.Core.Entities;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Sinav_Olusturma.Entities.Dtos
 {
-    public class StoryAddDto
+    public class StoryAddDto:IDto
     {
         public string Id { get; set; }
         public string Title { get; set; }
-        public string Content { get; set; }
-        public string Thumbnail { get; set; }
+
+
         public string Description { get; set; }
         public string Link { get; set; }
     }
