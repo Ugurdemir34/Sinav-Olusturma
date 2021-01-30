abe3fb34323acb4257ac872fb62e720e99598078
ugurdemir <ugurdemir551@gmail.com>
2021-01-30 03:38:14 +0300

    Temp

--- Sinav-Olusturma/Controllers/QuestionController.cs
+++ Sinav-Olusturma/Controllers/QuestionController.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Sinav_Olusturma.Entities.Concrete;
 
 namespace Sinav_Olusturma.Controllers
 {
     public class QuestionController : Controller
     {
-        public IActionResult Index(string url)
+        
+        public IActionResult Index(string storyLink,string storyTitle)
         {
-            ViewData["link"] = url;
+            ViewData["storyLink"] = storyLink;
+            ViewData["storyTitle"] = storyTitle;
             return View();
         }
     }
