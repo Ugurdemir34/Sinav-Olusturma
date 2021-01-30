8b818fd229edad6b98142ab77573b33145f397bd
ugurdemir <ugurdemir551@gmail.com>
2021-01-30 03:38:29 +0300

    Creaed

---
+++ Sinav-Olusturma/Helper/ContentReader.cs
@@ -0,0 +1,27 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Sinav_Olusturma.Helper
+{
+    public static class ContentReader
+    {
+        public static string GetContent(string url)
+        {
+            var html = new HtmlDocument();
+            html.LoadHtml(new WebClient().DownloadString(url));
+            var root = html.DocumentNode;
+            var p = root.Descendants()
+                    .Where(n => n.GetAttributeValue("class", "").Equals("content"))
+                    .Single()
+                    .Descendants("p")
+                    .Single();
+                    
+            var content = p.InnerText;
+            return content;
+        }
+    }
+}
