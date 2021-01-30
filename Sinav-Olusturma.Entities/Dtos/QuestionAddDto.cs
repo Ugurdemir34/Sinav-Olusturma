06d401d5f197a35e57f7f587c50b4e9f6bbca972
ugurdemir <ugurdemir551@gmail.com>
2021-01-30 20:26:29 +0300

    Created

---
+++ Sinav-Olusturma.Entities/Dtos/QuestionAddDto.cs
@@ -0,0 +1,17 @@
+﻿using Sinav_Olusturma.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sinav_Olusturma.Entities.Dtos
+{
+    public class QuestionAddDto:IDto
+    {
+        public string Title { get; set; }
+        public string OptionA { get; set; }
+        public string OptionB { get; set; }
+        public string OptionC { get; set; }
+        public string OptionD { get; set; }
+        public string CorrectOption { get; set; }
+    }
+}
