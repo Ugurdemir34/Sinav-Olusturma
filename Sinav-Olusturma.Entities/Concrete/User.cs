79ff372961c382bf44beb85c479bf8de5cdedb20
ugurdemir <ugurdemir551@gmail.com>
2021-01-30 20:26:35 +0300

    Updated

--- Sinav-Olusturma.Entities/Concrete/User.cs
+++ Sinav-Olusturma.Entities/Concrete/User.cs
@@ -1,12 +1,16 @@
 ﻿using Sinav_Olusturma.Core.Entities;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace Sinav_Olusturma.Entities.Concrete
 {
     public class User : IEntity
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public string Name { get; set; }
         public string Surname { get; set; }
