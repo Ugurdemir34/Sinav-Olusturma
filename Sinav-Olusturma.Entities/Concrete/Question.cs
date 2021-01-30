0c7e954788b1bfddf45afa5a3ac6bc730b519a0d
ugurdemir <ugurdemir551@gmail.com>
2021-01-30 20:26:46 +0300

    Updated

--- Sinav-Olusturma.Entities/Concrete/Question.cs
+++ Sinav-Olusturma.Entities/Concrete/Question.cs
@@ -1,12 +1,18 @@
-﻿using System;
+﻿using Sinav_Olusturma.Core.Entities;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace Sinav_Olusturma.Entities.Concrete
 {
-    public class Question
+    public class Question:IEntity
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+        public string Title { get; set; }
         public string OptionA { get; set; }
         public string OptionB { get; set; }
         public string OptionC { get; set; }
--- Sinav-Olusturma.Entities/Concrete/Story.cs
+++ Sinav-Olusturma.Entities/Concrete/Story.cs
@@ -1,16 +1,20 @@
 ﻿using Sinav_Olusturma.Core.Entities;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace Sinav_Olusturma.Entities.Concrete
 {
     public class Story :IEntity
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public string Id { get; set; }
         public string Title { get; set; }
-        public string Content { get; set; }
-        public string Thumbnail { get; set; }
+       // public string Content { get; set; }
+        //public string Thumbnail { get; set; }
         public string Description { get; set; }
         public string Link { get; set; }
         //Navigation Property
