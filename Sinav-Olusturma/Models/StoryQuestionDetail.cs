bc79bfb3613752b814cd9211e77f39d98f08e131
ugurdemir <ugurdemir551@gmail.com>
2021-01-31 11:55:54 +0300

    Created

---
+++ Sinav-Olusturma/Models/StoryQuestionDetail.cs
@@ -0,0 +1,20 @@
+﻿using Sinav_Olusturma.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sinav_Olusturma.Models
+{
+    public class StoryQuestionDetail
+    {
+        public string StoryTitle { get; set; }
+        public string StoryLink { get; set; }
+        public Question Question1 { get; set; }
+        public Question Question2 { get; set; }
+        public Question Question3 { get; set; }
+        public Question Question4 { get; set; }
+       // public List<SelectListItem> StoryTitles { get; set; }
+        public Story Story { get; set; }
+    }
+}
