540cafe0d5ac7e9f9bfe2f2f4d93cc654e9b2243
Uğur Demir <ugurdemir551@gmail.com>
2021-06-27 00:14:27 +0300

    Question List Added

--- Sinav-Olusturma/Models/StoryQuestionDetail.cs
+++ Sinav-Olusturma/Models/StoryQuestionDetail.cs
@@ -8,13 +8,18 @@ namespace Sinav_Olusturma.Models
 {
     public class StoryQuestionDetail
     {
+        public StoryQuestionDetail()
+        {
+            Questions = new List<Question>();
+        }
         public string StoryTitle { get; set; }
         public string StoryLink { get; set; }
-        public Question Question1 { get; set; }
-        public Question Question2 { get; set; }
-        public Question Question3 { get; set; }
-        public Question Question4 { get; set; }
-       // public List<SelectListItem> StoryTitles { get; set; }
+        //public Question Question1 { get; set; }
+        //public Question Question2 { get; set; }
+        //public Question Question3 { get; set; }
+        //public Question Question4 { get; set; }
+        public List<Question> Questions { get; set; }
+        // public List<SelectListItem> StoryTitles { get; set; }
         public Story Story { get; set; }
     }
 }
