ae8f563048181f671f4b386f31d6c3ca1519d7bd
Uğur Demir <ugurdemir551@gmail.com>
2021-06-27 00:11:03 +0300

    WebClient DataReader Class Added

---
+++ Sinav-Olusturma/Helper/DataReader.cs
@@ -0,0 +1,39 @@
+﻿using Sinav_Olusturma.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Sinav_Olusturma.Helper
+{
+    public class DataReader
+    {
+        private static string URL = "https://www.wired.com";
+        public static List<Story> GetData()
+        {
+            var temp = new List<Story>();
+            WebClient wc = new WebClient();
+            HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+            wc.Headers.Add("user-agent", "Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko");
+            var data = wc.DownloadString(URL);
+            doc.LoadHtml(data);
+            var searching = doc.DocumentNode.SelectNodes("//li[@class='post-listing-list-item__post']");
+
+
+            for (int i = 0; i < 5; i++)
+            {
+                var title = searching[i].SelectSingleNode("//li[" + (i + 1) + "]/a/div/h5").InnerText;
+                var link = searching[i].SelectSingleNode("//li[" + (i + 1) + "]/a[@class='post-listing-list-item__link']").Attributes["href"].Value;
+                temp.Add(new Story
+                {
+                    Title = title,
+                    Link = link
+                });
+               
+               
+            }
+            return temp;
+        }
+    }
+}
